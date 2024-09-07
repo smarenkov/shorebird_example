98cf75dad621c64cb43e146750bc4978f58bfd00
Simon Marenkov <simon.marenkov@gmail.com>
2024-09-07 18:32:12 +0300

    Update ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
+
+final shorebirdCodePush = ShorebirdCodePush();
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final availability = shorebirdCodePush.isShorebirdAvailable();
+  late final version = shorebirdCodePush.currentPatchNumber();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Shorebird available: $availability'),
+              FutureBuilder(
+                future: version,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Text('Shorebird patch: ${snapshot.data}');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
